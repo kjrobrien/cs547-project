49f1dbccc9d7bdec6af9831498a3f72e236d6d12
Kevin O'Brien <kjrobrien@gmail.com>
2024-11-12 19:22:22 -0500

    get all switch games

--- scrape_data/scrape_games.py
+++ scrape_data/scrape_games.py
@@ -6,7 +6,7 @@ import argparse
 default_fields = 'age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites'
 
 # Filter for Platform:Nintendo Switch and Category:Main Game with at least some ratings
-default_filter = 'platforms=130 & category=0 & (rating_count > 0 | aggregated_rating_count > 0)'
+default_filter = 'platforms=(130) & category=0 & (rating_count > 0 | aggregated_rating_count > 0)'
 
 
 parser = argparse.ArgumentParser(description="Fetch IGDB games and save to file.")
