6dd2188f32a18570d0a5c11a847031a817c06e88
Kevin O'Brien <kjrobrien@gmail.com>
2024-11-25 19:11:36 -0500

    use perason correlation

--- collaborative_filtering/collaborative_filtering.py
+++ collaborative_filtering/collaborative_filtering.py
@@ -8,6 +8,8 @@ import numpy as np
 parser = argparse.ArgumentParser(description="Take a ratings CSV file a run collaborative filtering.")
 
 parser.add_argument("--input", required=True, help="Input CSV file")
+parser.add_argument("--game-slug", required=True, help="The game slug to get recommendations")
+parser.add_argument("--num-recommendations", required=True, help="Number of recommendations")
 
 args= parser.parse_args()
 
@@ -22,15 +24,12 @@ aggregated_df = df.groupby(['user_slug', 'game_slug']).rating.max().reset_index(
 
 user_game_matrix = aggregated_df.pivot(index="user_slug", columns="game_slug", values="rating").fillna(0)
 
-game_similarity = cosine_similarity(user_game_matrix.T)
+game_similarity_df = user_game_matrix.corr(method="pearson")
 
-# This is a similarity matrix (x,y) representing the similarity between game x and y.
-game_similarity_df = pandas.DataFrame(game_similarity, index=user_game_matrix.columns, columns=user_game_matrix.columns)
-
-def get_top_similar(slug, similarity_df, n=20):
+def get_top_similar(slug, similarity_df):
     similar = similarity_df[slug].sort_values(ascending=False)
-    return similar[1:n+1]
+    return similar[1:int(args.num_recommendations) + 1]
 
-similar_items = get_top_similar("super-mario-odyssey", game_similarity_df)
+similar_items = get_top_similar(args.game_slug, game_similarity_df)
 
 print(similar_items)
