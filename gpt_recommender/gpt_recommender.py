e796e4de00de5cfc9bf4dd33b3d24e9697ccab38
Kevin O'Brien <kjrobrien@gmail.com>
2024-12-07 13:23:34 -0500

    clean up instructions, filter out hallucinations

--- gpt_recommender/gpt_recommender.py
+++ gpt_recommender/gpt_recommender.py
@@ -9,14 +9,24 @@ parser = argparse.ArgumentParser(description="Take a ratings CSV file and a game
 
 parser.add_argument("--games-input", required=True, help="Input games JSON file")
 parser.add_argument("--game-slugs", required=True, help="The game slug to get recommendations")
+parser.add_argument("--num-games", required=True, help="The number of games to recommend")
 parser.add_argument("--openai-api-key", required=True, help="API Key for Open AI")
 
 args= parser.parse_args()
 
+def convert_slugs_to_tag_strings(slugs):
+    slugs_with_tags = []
+    for slug in slugs:
+        slugs_with_tags.extend(["<SLUG>"+slug+"</SLUG>"])
+    return "\n".join(slugs_with_tags)
 
+
+class SlugWithScore(BaseModel):
+    slug: str
+    score: float
 
 class GameResponse(BaseModel):
-    slugs: list[str]
+    slugs: list[SlugWithScore]
     
 client = OpenAI(
     api_key=args.openai_api_key
@@ -27,21 +37,36 @@ games = []
 with open(args.games_input, "r") as file:
   games = json.load(file)
   
-slugs = []
+known_slugs = []
   
 for game in games:
-    slugs.extend([game["slug"]])
+    known_slugs.extend([game["slug"]])
 
-slugs_string = ",".join(slugs)
+
+user_slugs = args.game_slugs.split(",")
+
 completion = client.beta.chat.completions.parse(
     messages=[
         {
             "role": "system",
-            "content": f"You are a helpful video game recommender. You will give recommendations for new games to the user given their preferences expressed as slugs. Return just the recommended game slugs. Only return games slugs from the following list, do not return any extras. Return these exact slugs. The list of slugs: {slugs_string}",
+            "content":
+f'''<INSTRUCTIONS>You are a helpful video game recommender. You have existing knowledge of all Nintendo Switch games.
+
+You will receive multiple tags:
+
+<KNOWN_SLUGS> - this contains all of the known game slugs that correspond to Nintendo Switch games. You will only recommend games that are included in this tag. Each game is wrapped in a <SLUG> tag.
+<USER_SLUGS> - this contains the game slugs that we know the user likes. You will recommend other games from <KNOWN_SLUGS> that you think the user will like based on <USER_SLUGS>. Each game is wrapped in a <SLUG> tag. You will include a rating score between 0 and 1 for the user in the response.
+<NUM_GAMES> - this contains the number of games the user would like to have returned as recommendations.
+</INSTRUCTIONS>
+
+<KNOWN_SLUGS>
+{convert_slugs_to_tag_strings(known_slugs)}
+
+</KNOWN_SLUGS>''',
         },
         {
             "role": "user",
-            "content": f"My favorite game slugs are: {args.game_slugs}"
+            "content": f"<USER_SLUGS>{convert_slugs_to_tag_strings(user_slugs)}</USER_SLUGS><NUM_GAMES>{args.num_games}</NUM_GAMES>"
         }
     ],
     model="gpt-4o-mini",
@@ -49,4 +74,9 @@ completion = client.beta.chat.completions.parse(
 )
 
 recommendations = completion.choices[0].message.parsed
-print(recommendations)
+
+
+# Filter out any slug hallucinations, don't include any slugs the user provided
+filtered_slugs = [x for x in recommendations.slugs if x.slug in known_slugs and x.slug not in user_slugs]
+
+print(filtered_slugs)
